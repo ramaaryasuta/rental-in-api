4fae8f523ba01e8f1c8783059989512a8df8779e
Rama Aryasuta <ramaaryasutaweihan@gmail.com>
2025-05-12 21:57:58 +0700

    init commit

---
+++ src/main/java/com/ralabs/rental_in/RentalInApplication.java
@@ -0,0 +1,13 @@
+package com.ralabs.rental_in;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RentalInApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RentalInApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ralabs/rental_in/RentalInApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ralabs.rental_in;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RentalInApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
